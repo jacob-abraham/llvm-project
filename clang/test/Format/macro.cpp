0ee83b000a76f0b866920944316aefee6f0ce61e
jacob-abraham <jacob.r.abraham@gmail.com>
2022-10-09 14:20:37 -0700

    remove bad test

--- clang/test/Format/macro.cpp
+++
@@ -1,5 +0,0 @@
-// RUN: clang-format -style="{AllowShortCaseLabelsOnASingleLine: true}" %s > %t
-// RUN: diff %s %t
-#define X                                                                      \
-  case 0: break;
-#include "f"
